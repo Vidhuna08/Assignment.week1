570d1e12046918e9c8871c9bf8733e684f860d0d
Vidhuna08 <112848233+Vidhuna08@users.noreply.github.com>
2022-09-08 15:10:30 +0530

    Add files via upload

---
+++ day1/Car.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Car {
+	
+	public void applyBrake() {
+		
+		System.out.println("applied brake");
+		
+	}
+	
+	public void soundHorn() {
+		
+		System.out.println("sound horn");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ day1/Student.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Student {
+	
+	String studentName="Vidhuna";
+	int rollno=8;
+	String CollegeName="PSG College";
+    int MarkScored=500;
+    float cgpa=9.8f;
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
